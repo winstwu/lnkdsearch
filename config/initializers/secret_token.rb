77b3e219944bb96568a7e5aef506522a2c2e04d8
Winston W <winstwu@gmail.com>
2013-05-14 15:18:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lnkdsearch::Application.config.secret_token = '76d524c2afe1a5c5c364763c239982fa8c261d028d6183711d76092f30b3a732c1122e0d799dfad2af919bfc31be7e25738048b6aaafd5e84abf8f1a8d56c6ea'
