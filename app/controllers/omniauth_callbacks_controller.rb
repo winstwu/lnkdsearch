03663672e28ef1323295592869f091999c186c2f
Winston W <winstwu@gmail.com>
2013-05-20 16:49:37 -0400

    Add Linkedin API authorization

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+	@@config = { 
+    :site => 'https://api.linkedin.com',
+    :authorize_path => '/uas/oauth/authenticate',
+    :request_token_path => '/uas/oauth/requestToken?scope=r_basicprofile+r_fullprofile',   
+    :access_token_path => '/uas/oauth/accessToken' 
+  }
+
 	def all
 		user = User.from_omniauth(request.env["omniauth.auth"])
 		session[:usertest] = user
 		session[:token] = user.token
 		session[:secret] = user.secret
+		client = LinkedIn::Client.new('gu7dbvwzvog2', 'ENg82itfHPKuSTnR')
+		session[:pin] = client.request_token.authorize_url
+		client.authorize_from_access(session[:token], session[:secret])
+		client
 		#raise request.env["omniauth.auth"].to_yaml
 		if user.persisted?
 			flash.notice = "Signed in!"			
@@ -14,4 +25,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		end
 	end
 	alias_method :linkedin, :all
+
+	def oauth_account
+	  client = LinkedIn::Client.new('gu7dbvwzvog2', 'ENg82itfHPKuSTnR', @@config)
+	  pin = params[:oauth_verifier]
+	  if pin
+	    atoken, asecret = client.authorize_from_request(session[:rtoken], session[:rsecret], pin)
+	    LinkedinOauthSetting.create!(:asecret => asecret, :atoken => atoken, :user_id => current_user.id)
+	  end
+	  redirect_to "/"
+	end
 end
